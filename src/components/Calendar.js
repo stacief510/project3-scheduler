1e59edb7972134412a36e0fa7910009c9c211102
stacief510 <staciemfraser@gmail.com>
2018-05-10 13:31:53 -0700

    created calendar view

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -5,12 +5,22 @@ class Calendar extends Component{
 	render(){
 		return(
 			<div className="calendar">
-				<p>Calendar Page</p>
-				<p>Monday</p>
-				<p>Tuesday</p>
-				<p>Wednesday</p>
-				<p>Thursday</p>
-				<p>Friday</p>
+				<div className="container">
+					<div className="row">
+						<div className="col-sm">Monday</div>
+						<div className="col-sm">Tuesday</div>
+						<div className="col-sm">Wednesday</div>
+						<div className="col-sm">Thursday</div>
+						<div className="col-sm">Friday</div>
+					</div>
+					<div className="row">
+						<div className="col-sm row2"></div>
+						<div className="col-sm row2"></div>
+						<div className="col-sm row2">Appt 1</div>
+						<div className="col-sm row2"></div>
+						<div className="col-sm row2"></div>
+					</div>
+				</div>
 			</div>
 			)
 	}
